a240094899c06911c06898bae23f8ebe5f78d1b1
Nati <natijago@gmail.com>
2026-09-18 00:02:22 -0300

    Feat: creacion de entidad project y task P2, interceptor y CQRS

---
+++ src/ProjectHub.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectHub.Application.Projects.Commands.CreateProject;
+using ProjectHub.Application.Projects.Common;
+using ProjectHub.Application.Projects.Queries.GetProjects;
+
+namespace ProjectHub.Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ProjectsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<ProjectDto>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<ProjectDto>>> GetAll()
+    {
+        var result = await _sender.Send(new GetProjectsQuery());
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ProjectDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ProjectDto>> Create([FromBody] CreateProjectCommand command)
+    {
+        var result = await _sender.Send(command);
+        return Ok(result);
+    }
+}
